61972026caf108bd5dcb58f0d42a8fd996e2ab04
Paul Kwiatkowski <paul@groupraise.com>
2014-10-11 17:44:20 -0500

    Initial Rails Commit.

---
+++ rireki/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rireki::Application.config.secret_key_base = '2ee957cdd75ab430a9223617ea71595a46c55be684ede6f29d29e3e85fe654ab6bfce627afc3819008b0c2e5ebe4216410c34c2434db080935feea2cd057aa54'
