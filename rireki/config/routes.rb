33f1a7614a275e4fc4c7d02c8f8c9ebd12a6d1bb
Paul Kwiatkowski <paul@groupraise.com>
2014-10-11 19:00:14 -0500

    Added devise with some config options, created user model and added login routes.

--- rireki/config/routes.rb
+++ rireki/config/routes.rb
@@ -1,5 +1,19 @@
 Rireki::Application.routes.draw do
 
+  devise_for :users
+
+  devise_scope :user do
+    get "sign_in",  to: "devise/sessions#new"
+    get "signin",   to: "devise/sessions#new"
+    get "log_in",   to: "devise/sessions#new"
+    get "login",    to: "devise/sessions#new"
+
+    get "sign_out", to: "devise/sessions#destroy"
+    get "signout",  to: "devise/sessions#destroy"
+    get "log_out",  to: "devise/sessions#destroy"
+    get "logout",   to: "devise/sessions#destroy"
+  end
+
   resources :qualifications
 
   resources :activities
